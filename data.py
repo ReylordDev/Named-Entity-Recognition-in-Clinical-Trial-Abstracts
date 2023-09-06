3441230e05b5d40ea82306b9a5cf960dc9d5af78
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-09-06 08:29:47 +0200

    rnn main

--- data.py
+++ data.py
@@ -1,263 +1,346 @@
 import json
+import random
 import torch
 from torch.utils.data import Dataset, DataLoader
-from collections import defaultdict
 
+# Global variables
 TRAIN_DATA_PATH = "data/train.json"
 TEST_DATA_PATH = "data/test.json"
 PAD = "<PAD>"
 
 
-class ClinicalTrialsDataset(Dataset):
-    def __init__(self, sentences, labels):
-        """
-        Constructor method to initialize the dataset with sentences and labels.
+# Preprocessing Functions
+def load_data(train_file_path, test_file_path):
+    """
+    Load JSON data from the given file paths.
 
-        Args:
-        - sentences (list of lists): List of tokenized sentences, where each sentence is a list of words.
-        - labels (list of lists): List of labels associated with each sentence.
-        """
+    Parameters:
+    train_file_path (str): Path to the training JSON file.
+    test_file_path (str): Path to the testing JSON file.
+
+    Returns:
+    tuple: Loaded JSON data for training and testing.
+    """
+    with open(train_file_path, "r") as f:
+        train_data = json.load(f)
+    with open(test_file_path, "r") as f:
+        test_data = json.load(f)
+    return train_data, test_data
+
+
+def extract_sentences_and_labels(data):
+    """
+    Extract sentences and raw labels from the loaded JSON data.
+
+    Parameters:
+    data (list): Loaded JSON data.
+
+    Returns:
+    tuple: Lists of sentences and corresponding raw label data.
+    """
+    sentences = []
+    labels = []
+    for entry in data:
+        for sentence in entry["sentences"]:
+            sentences.append(sentence["words"])
+            labels.append(sentence["entities"])
+    return sentences, labels
+
+
+def generate_label_vocab(labels):
+    """
+    Generate a list of unique labels from the raw label data.
+
+    Parameters:
+    labels (list): Raw label data.
+
+    Returns:
+    list: A list of unique labels found in the dataset.
+    """
+    unique_labels = set()
+    for sentence_labels in labels:
+        for label in sentence_labels:
+            unique_labels.add(label["label"])
+    return list(unique_labels)
+
+
+def encode_sentences(sentences, word_to_idx):
+    encoded_sentences = []
+    for sentence in sentences:
+        encoded_sentence = [
+            word_to_idx.get(word, word_to_idx.get("<UNK>")) for word in sentence
+        ]
+        encoded_sentences.append(encoded_sentence)
+    return encoded_sentences
+
+
+def build_word_to_idx(sentences):
+    word_to_idx = {"<PAD>": 0, "<UNK>": 1}
+    idx = 2
+    for sentence in sentences:
+        for word in sentence:
+            if word not in word_to_idx:
+                word_to_idx[word] = idx
+                idx += 1
+    return word_to_idx
+
+
+def build_idx_to_word(word_to_idx):
+    return {idx: word for word, idx in word_to_idx.items()}
+
+
+def build_label_to_idx(label_vocab):
+    return {label: idx for idx, label in enumerate(label_vocab)}
+
+
+def build_idx_to_label(label_to_idx):
+    return {idx: label for label, idx in label_to_idx.items()}
+
+
+def encode_labels(labels, label_vocab, sentences):
+    """
+    Convert the raw label data to a multi-hot encoded format.
+
+    Parameters:
+    labels (list): Raw label data.
+    label_vocab (list): Vocabulary of unique labels.
+
+    Returns:
+    list: Multi-hot encoded labels for each token in the sentences.
+    """
+    encoded_labels = []
+    for sentence_labels, sentence in zip(labels, sentences):
+        sentence_encoded = []
+        for i in range(len(sentence)):  # Enumerate over tokens in the sentence
+            token_encoded = [0] * len(label_vocab)  # Initialize with zeros
+            for label_span in sentence_labels:
+                # Check if the current token is within the label span
+                if i >= label_span["start_pos"] and i <= label_span["end_pos"]:
+                    index = label_vocab.index(label_span["label"])
+                    token_encoded[index] = 1
+            sentence_encoded.append(token_encoded)
+        encoded_labels.append(sentence_encoded)
+    return encoded_labels
+
+
+def pad_sequences(sequences, pad_value, max_length):
+    """
+    Pad the tokenized sentences and labels to a fixed length.
+
+    Parameters:
+    sequences (list): List of tokenized sentences or labels.
+    pad_value (str or int): Value to use for padding.
+    max_length (int): Maximum length to pad the sequences to.
+
+    Returns:
+    list: Padded sentences or labels.
+    """
+    padded_sequences = []
+    for sequence in sequences:
+        if len(sequence) >= max_length:
+            padded_sequences.append(sequence[:max_length])
+        else:
+            padded_sequence = sequence + [pad_value] * (max_length - len(sequence))
+            padded_sequences.append(padded_sequence)
+    return padded_sequences
+
+
+def find_max_length(sentences):
+    """
+    Find the maximum length among all sentences.
+
+    Parameters:
+    sentences (list): List of tokenized sentences.
+
+    Returns:
+    int: Maximum length among all sentences.
+    """
+    return max(len(sentence) for sentence in sentences)
+
+
+class NERDataset(Dataset):
+    def __init__(self, sentences, labels):
         self.sentences = sentences
         self.labels = labels
 
     def __len__(self):
-        """
-        Returns the total number of samples in the dataset.
-        """
         return len(self.sentences)
 
     def __getitem__(self, idx):
-        """
-        Given an index idx, returns the sentence and its corresponding label at that index.
-
-        Args:
-        - idx (int): Index of the desired sample.
-
-        Returns:
-        - tuple: A tuple containing the sentence and its associated label at the given index.
-        """
-        return self.sentences[idx], self.labels[idx]
+        return {
+            "sentence": torch.tensor(self.sentences[idx], dtype=torch.long),
+            "label": torch.tensor(self.labels[idx], dtype=torch.float),
+        }
 
 
-def load_data(
-    train_data_path=TRAIN_DATA_PATH, test_data_path=TEST_DATA_PATH, batch_size=32
+def split_data(sentences, labels, split_ratio=0.9):
+    """
+    Split the data into training and validation sets
+    """
+    # Determine the split index
+    split_idx = int(len(sentences) * split_ratio)
+
+    # Split the sentences and labels
+    train_sentences = sentences[:split_idx]
+    val_sentences = sentences[split_idx:]
+    train_labels = labels[:split_idx]
+    val_labels = labels[split_idx:]
+
+    return train_sentences, train_labels, val_sentences, val_labels
+
+
+def create_data_loaders(
+    train_sentences,
+    train_labels,
+    val_sentences,
+    val_labels,
+    test_sentences,
+    test_labels,
+    batch_size=32,
 ):
-    # Load the datasets from the provided JSON files
-    with open(train_data_path, "r") as train_file:
-        train_dataset = json.load(train_file)
+    """
+    Create DataLoaders for training, validation, and test datasets
+    """
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    generator = torch.Generator(device=device)
+    # Create training, validation, and test datasets
+    train_dataset = NERDataset(train_sentences, train_labels)
+    val_dataset = NERDataset(val_sentences, val_labels)
+    test_dataset = NERDataset(test_sentences, test_labels)
 
-    with open(test_data_path, "r") as test_file:
-        test_dataset = json.load(test_file)
-
-    (
-        train_sentences,
-        train_labels,
-        test_sentences,
-        test_labels,
-        word_to_id,
-        label_to_id,
-        max_sequence_length,
-    ) = preprocess_data(train_dataset, test_dataset)
-
-    train_loader, test_loader = get_dataloaders(
-        train_sentences, train_labels, test_sentences, test_labels, batch_size
+    # Create DataLoaders for each dataset
+    train_loader = DataLoader(
+        train_dataset, shuffle=True, batch_size=batch_size, generator=generator
+    )
+    val_loader = DataLoader(
+        val_dataset, shuffle=False, batch_size=batch_size, generator=generator
+    )
+    test_loader = DataLoader(
+        test_dataset, shuffle=False, batch_size=batch_size, generator=generator
     )
 
-    print("Data loaded.")
-
-    return train_loader, test_loader, word_to_id, label_to_id, max_sequence_length
+    return train_loader, val_loader, test_loader
 
 
-def preprocess_data(train_dataset, test_dataset):
-    (
-        train_sentences,
-        train_labels,
-        test_sentences,
-        test_labels,
-    ) = extract_sentences_and_labels(train_dataset, test_dataset)
+# Utility Functions
+def sample_data(sentences, labels, sample_size=5):
+    combined = list(zip(sentences, labels))
+    random.shuffle(combined)
+    sampled_sentences, sampled_labels = zip(*random.sample(combined, sample_size))
+    return list(sampled_sentences), list(sampled_labels)
 
-    word_to_id, label_to_id = create_vocabulary(
-        train_sentences, train_labels, test_sentences, test_labels
+
+def tensor_to_sentences(tensor, idx_to_word):
+    decoded_sentences = []
+    for sentence in tensor:
+        decoded_sentence = [idx_to_word[idx.item()] for idx in sentence]
+        decoded_sentences.append((decoded_sentence))
+    return decoded_sentences
+
+
+def tensor_to_labels(tensor, idx_to_label):
+    decoded_labels = []
+    for sentence_labels in tensor:
+        decoded_sentence_labels = []
+        for token_labels in sentence_labels:
+            token_decoded_labels = [
+                idx_to_label[idx]
+                for idx, value in enumerate(token_labels)
+                if value == 1
+            ]
+            decoded_sentence_labels.append(token_decoded_labels)
+        decoded_labels.append(decoded_sentence_labels)
+    return decoded_labels
+
+
+def prepare_data_pipeline(
+    train_file_path=TRAIN_DATA_PATH, test_file_path=TEST_DATA_PATH, debug=False
+):
+    # Load data
+    train_data, test_data = load_data(train_file_path, test_file_path)
+
+    # Extract sentences and labels
+    train_sentences, train_raw_labels = extract_sentences_and_labels(train_data)
+    test_sentences, test_raw_labels = extract_sentences_and_labels(test_data)
+
+    # Generate label vocabulary
+    label_vocab = generate_label_vocab(train_raw_labels + test_raw_labels)
+
+    # Encode labels
+    train_encoded_labels = encode_labels(train_raw_labels, label_vocab, train_sentences)
+    test_encoded_labels = encode_labels(test_raw_labels, label_vocab, test_sentences)
+
+    # Build the conversion dictionaries
+    word_to_idx = build_word_to_idx(train_sentences + test_sentences)
+    idx_to_word = build_idx_to_word(word_to_idx)
+    label_to_idx = build_label_to_idx(label_vocab)
+    idx_to_label = build_idx_to_label(label_to_idx)
+
+    # Encode sentences
+    train_encoded_sentences = encode_sentences(train_sentences, word_to_idx)
+    test_encoded_sentences = encode_sentences(test_sentences, word_to_idx)
+
+    # Find max length for dynamic padding
+    MAX_LENGTH = find_max_length(train_sentences + test_sentences)
+
+    # Pad sequences
+    padded_train_sentences = pad_sequences(train_encoded_sentences, 0, MAX_LENGTH)
+    padded_train_labels = pad_sequences(
+        train_encoded_labels, [0] * len(label_vocab), MAX_LENGTH
     )
 
-    (
-        train_sentences_padded,
-        train_labels_int,
-        test_sentences_padded,
-        test_labels_int,
-        max_sequence_length,
-    ) = convert_and_pad_data(
+    padded_test_sentences = pad_sequences(test_encoded_sentences, 0, MAX_LENGTH)
+    padded_test_labels = pad_sequences(
+        test_encoded_labels, [0] * len(label_vocab), MAX_LENGTH
+    )
+
+    if debug:
+        # Print example data
+        example_idx = 1  # Change this to see different examples
+
+        print(
+            f"Train data length: {len(train_sentences)}, Test data length: {len(test_sentences)}"
+        )
+        print(f"Example sentence: {' '.join(train_sentences[example_idx])}")
+        print(f"Example labels: {train_raw_labels[example_idx]}")
+
+        print(f"Encoded sentence: {train_encoded_sentences[example_idx]}")
+        print(f"Encoded labels: {train_encoded_labels[example_idx]}")
+
+        print(f"Padded sentence: {padded_train_sentences[example_idx]}")
+        print(f"Padded labels: {padded_train_labels[example_idx]}")
+
+    # Split training data into training and validation sets
+    train_sentences, train_labels, val_sentences, val_labels = split_data(
+        padded_train_sentences, padded_train_labels
+    )
+
+    if debug:
+        print(
+            f"Train data length: {len(train_sentences)}, Validation data length: {len(val_sentences)}"
+        )
+
+    # Create DataLoaders
+    train_loader, val_loader, test_loader = create_data_loaders(
         train_sentences,
         train_labels,
-        test_sentences,
-        test_labels,
-        word_to_id,
-        label_to_id,
+        val_sentences,
+        val_labels,
+        padded_test_sentences,
+        padded_test_labels,
     )
 
     return (
-        train_sentences_padded,
-        train_labels_int,
-        test_sentences_padded,
-        test_labels_int,
-        word_to_id,
-        label_to_id,
-        max_sequence_length,
+        train_loader,
+        val_loader,
+        test_loader,
+        MAX_LENGTH,
+        word_to_idx,
+        idx_to_word,
+        label_to_idx,
+        idx_to_label,
     )
-
-
-def extract_sentences_and_labels(train_dataset, test_dataset):
-    # Extract sentences and labels from the datasets
-    train_sentences = []
-    train_labels = []
-    for abstract in train_dataset:
-        for sentence in abstract["sentences"]:
-            train_sentences.append(sentence["words"])
-            train_labels.append([entity["label"] for entity in sentence["entities"]])
-
-    test_sentences = []
-    test_labels = []
-    for abstract in test_dataset:
-        for sentence in abstract["sentences"]:
-            test_sentences.append(sentence["words"])
-            test_labels.append([entity["label"] for entity in sentence["entities"]])
-
-    print("Data extracted.")
-    print("Train sentence count: ", len(train_sentences))
-    print("Test sentence count: ", len(test_sentences))
-
-    return train_sentences, train_labels, test_sentences, test_labels
-
-
-def create_vocabulary(train_sentences, train_labels, test_sentences, test_labels):
-    # Create a vocabulary of unique words from train and test datasets
-    word_to_id = defaultdict(lambda: len(word_to_id))
-    word_to_id[PAD]  # Initialize with padding token
-
-    # Populate the vocabulary
-    for sentence in train_sentences + test_sentences:
-        for word in sentence:
-            word_to_id[word]
-
-    # Create a set of unique labels to convert labels into integers
-    label_to_id = defaultdict(lambda: len(label_to_id))
-    for label_list in train_labels + test_labels:
-        for label in label_list:
-            label_to_id[label]
-
-    print("Vocabulary and label set created.")
-    print("Vocabulary size: ", len(word_to_id))
-    print("Label set size: ", len(label_to_id) + 1)  # +1 for the padding token
-
-    return word_to_id, label_to_id
-
-
-def convert_and_pad_data(
-    train_sentences, train_labels, test_sentences, test_labels, word_to_id, label_to_id
-):
-    # Convert sentences to their corresponding integer values
-    train_sentences_int = [
-        [word_to_id[word] for word in sentence] for sentence in train_sentences
-    ]
-    test_sentences_int = [
-        [word_to_id[word] for word in sentence] for sentence in test_sentences
-    ]
-
-    # Convert labels to their corresponding integer values
-    train_labels_padded = [
-        [label_to_id[label] for label in label_list] for label_list in train_labels
-    ]
-    test_labels_padded = [
-        [label_to_id[label] for label in label_list] for label_list in test_labels
-    ]
-
-    # Pad sequences to have the same length
-    max_sequence_length = max(
-        max(len(sentence) for sentence in train_sentences_int),
-        max(len(sentence) for sentence in test_sentences_int),
-    )
-    train_sentences_padded = [
-        sentence + [word_to_id[PAD]] * (max_sequence_length - len(sentence))
-        for sentence in train_sentences_int
-    ]
-    test_sentences_padded = [
-        sentence + [word_to_id[PAD]] * (max_sequence_length - len(sentence))
-        for sentence in test_sentences_int
-    ]
-
-    # Pad the labels to the same max sequence length as the sentences
-    train_labels_padded = [
-        label_list + [label_to_id[PAD]] * (max_sequence_length - len(label_list))
-        for label_list in train_labels_padded
-    ]
-    test_labels_padded = [
-        label_list + [label_to_id[PAD]] * (max_sequence_length - len(label_list))
-        for label_list in test_labels_padded
-    ]
-
-    print("Data converted and padded.")
-    print("Max sequence length: ", max_sequence_length)
-
-    return (
-        train_sentences_padded,
-        train_labels_padded,
-        test_sentences_padded,
-        test_labels_padded,
-        max_sequence_length,
-    )
-
-
-def get_dataloaders(
-    train_sentences_padded,
-    train_labels_padded,
-    test_sentences_padded,
-    test_labels_padded,
-    batch_size,
-):
-    # Convert data into PyTorch tensors and set up dataloaders
-    train_sentences_tensor = torch.tensor(train_sentences_padded)
-    test_sentences_tensor = torch.tensor(test_sentences_padded)
-    train_labels_tensor = torch.tensor(train_labels_padded)
-    test_labels_tensor = torch.tensor(test_labels_padded)
-    train_dataset = ClinicalTrialsDataset(train_sentences_tensor, train_labels_tensor)
-    test_dataset = ClinicalTrialsDataset(test_sentences_tensor, test_labels_tensor)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-    print("Dataloaders set up.")
-    print("Train dataset size: ", len(train_dataset))
-    print("Test dataset size: ", len(test_dataset))
-
-    return train_loader, test_loader
-
-
-def ids_to_sentences(ids, id_to_word):
-    """
-    Given a tensor of shape [seq_length], returns the corresponding sentence as a string.
-
-    Args:
-    - ids (torch.Tensor): Tensor of shape [seq_length] containing the word ids of the sentence.
-    - id_to_word (dict): Dictionary mapping word ids to words.
-
-    Returns:
-    - str: The sentence corresponding to the given word ids.
-    """
-    return " ".join([id_to_word[id] for id in ids])
-
-
-def ids_to_labels(ids, id_to_label):
-    """
-    Given a tensor of shape [seq_length], returns the corresponding labels as a string.
-
-    Args:
-    - ids (torch.Tensor): Tensor of shape [seq_length] containing the label ids of the sentence.
-    - id_to_label (dict): Dictionary mapping label ids to labels.
-
-    Returns:
-    - str: The labels corresponding to the given label ids.
-    """
-    return " ".join([id_to_label[id] for id in ids])
 
 
 if __name__ == "__main__":
-    load_data()
+    prepare_data_pipeline(TRAIN_DATA_PATH, TEST_DATA_PATH, debug=True)
